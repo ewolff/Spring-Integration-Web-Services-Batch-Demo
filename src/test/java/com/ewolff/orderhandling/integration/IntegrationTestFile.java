09202dba5d4f6265a6e0a15596473adc0c839af8
Eberhard Wolff <eberhard.wolff@gmail.com>
2013-03-04 09:31:22 +0100

    Router now uses SpEL in XML instead of Java code

--- src/test/java/com/ewolff/orderhandling/integration/IntegrationTestFile.java
+++ src/test/java/com/ewolff/orderhandling/integration/IntegrationTestFile.java
@@ -21,17 +21,10 @@ public class IntegrationTestFile extends AbstractSpringBasedTest {
 	@Autowired
 	private ExpressFulFillmentJms expressFulFillmentJms;
 
-	private PrintWriter printWriter;
-
-	@Before
-	public void setUp() throws Exception {
-		
-	}
-
 	@Test
 	public void testNormalOrder() throws Exception {
 		int fulFillmentBefore = normalFulFillment.getOrders().size();
-		printWriter = new PrintWriter("/tmp/files/orders" + System.currentTimeMillis());
+		PrintWriter printWriter = new PrintWriter("/tmp/files/orders" + System.currentTimeMillis());
 		printWriter.write("21,1,iPod,normal,101,");
 		printWriter.flush();
 		printWriter.close();
@@ -49,7 +42,7 @@ public class IntegrationTestFile extends AbstractSpringBasedTest {
 	public void testExpressOrder() throws Exception {
 		int fulFillmentBeforeJms = expressFulFillmentJms.getOrders().size();
 		File file = new File("/tmp/files/orders" + System.currentTimeMillis());
-		printWriter = new PrintWriter(file);
+		PrintWriter printWriter = new PrintWriter(file);
 		printWriter.write("20,1,iPod,express,102,");
 		printWriter.close();
 		int i = 0;
